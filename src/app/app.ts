490103a541c2767aba8527dce6a087eb42568058
Ayaanmaniar <ayaanmaniar085@gmail.com>
2026-02-13 16:13:00 +0500

    update code

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { Header } from './component/header/header';
+import { Footer } from './component/footer/footer';
+
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone : true,
+  imports: [RouterOutlet,Header,Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
